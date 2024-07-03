8666fa22cece2dc4d48838da7f1a2e4978f9f324
Fabian Suarez <inge.fabiansuarez@gmail.com>
2024-07-03 17:23:55 -0500

    DICEAPP Se hace el primer diseño de la app, falta agregar la logica

--- LanzamientoDadosApp/app/src/main/java/me/fabiansuarez/lanzamientodadosapp/MainActivity.kt
+++ LanzamientoDadosApp/app/src/main/java/me/fabiansuarez/lanzamientodadosapp/MainActivity.kt
@@ -4,12 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import me.fabiansuarez.lanzamientodadosapp.ui.theme.LanzamientoDadosAppTheme
 
@@ -18,30 +26,34 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            LanzamientoDadosAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            MyApp()
         }
     }
+
+
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+private fun MyApp() {
+    DiceWithButtonAndImage()
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+private fun DiceWithButtonAndImage(modifier: Modifier = Modifier.fillMaxSize()) {
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    LanzamientoDadosAppTheme {
-        Greeting("Android")
+    var result = 1
+    val painter = painterResource(id = R.drawable.dice_1)
+    Column(
+        modifier = modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Image(painter = painter, contentDescription = null)
+        Button(onClick = {
+            result = (1..6).random()
+        }) {
+            Text(text = stringResource(R.string.roll_dice))
+        }
     }
 }
