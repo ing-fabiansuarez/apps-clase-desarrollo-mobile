b5639f2b889450fe2dd03d41f726e905e34077a4
Fabian Suarez <inge.fabiansuarez@gmail.com>
2024-07-03 21:15:03 -0500

    manejo de estado local en la misma funcion composable o componible

--- stateCompose/calculadoraivaapp/app/src/main/java/me/fabiansuarez/calculadoraivaapp/MainActivity.kt
+++ stateCompose/calculadoraivaapp/app/src/main/java/me/fabiansuarez/calculadoraivaapp/MainActivity.kt
@@ -14,6 +14,8 @@ import androidx.compose.foundation.layout.safeContentPadding
 import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -21,9 +23,13 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusTargetModifierNode
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import me.fabiansuarez.calculadoraivaapp.ui.theme.CalculadoraIVAAppTheme
@@ -63,7 +69,11 @@ fun FormTaxesLayout() {
                 .padding(bottom = 16.dp, top = 40.dp)
                 .align(alignment = Alignment.Start)
         )
-        EditNumberField(modifier = Modifier.padding(bottom = 32.dp).fillMaxWidth())
+        EditNumberField(
+            modifier = Modifier
+                .padding(bottom = 32.dp)
+                .fillMaxWidth()
+        )
         Text(
             text = "Calculo de impuesto: $0.00",
             style = MaterialTheme.typography.headlineSmall
@@ -74,13 +84,31 @@ fun FormTaxesLayout() {
 
 @Composable
 fun EditNumberField(modifier: Modifier = Modifier) {
+    //var amountInput = "0"
+    var amountInput = remember {
+        mutableStateOf("")
+    }
 
-    var amountInput = "0"
+    val amount = amountInput.value.toDoubleOrNull() ?: 0.0
 
+    val tax = calculateTax(amount)
+
+
+    /*TextField(
+        value = amountInput.value,
+        .....
+    )*/
     TextField(
-        value = amountInput,
-        onValueChange = {},
-        modifier = modifier
+        value = amountInput.value,
+        onValueChange = {
+            amountInput.value = it
+        },
+        label = {
+                Text(text = "Cantidad a calcular")
+        },
+        singleLine = true,//Esto condensa el cuadro de texto en una sola línea desplazable horizontalmente a partir de varias líneas.
+        modifier = modifier,
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
     )
 }
 
