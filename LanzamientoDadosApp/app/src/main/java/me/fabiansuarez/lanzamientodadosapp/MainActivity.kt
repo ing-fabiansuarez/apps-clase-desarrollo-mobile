2c37ce28daf071b58e1550eb6ff87def1d83483e
Fabian Suarez <inge.fabiansuarez@gmail.com>
2024-07-03 19:28:05 -0500

    DICEAPP se agrego la funcionalidad del dado...

--- LanzamientoDadosApp/app/src/main/java/me/fabiansuarez/lanzamientodadosapp/MainActivity.kt
+++ LanzamientoDadosApp/app/src/main/java/me/fabiansuarez/lanzamientodadosapp/MainActivity.kt
@@ -14,6 +14,10 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
@@ -29,8 +33,6 @@ class MainActivity : ComponentActivity() {
             MyApp()
         }
     }
-
-
 }
 
 @Preview(showBackground = true, showSystemUi = true)
@@ -41,15 +43,24 @@ private fun MyApp() {
 
 @Composable
 private fun DiceWithButtonAndImage(modifier: Modifier = Modifier.fillMaxSize()) {
-
-    var result = 1
-    val painter = painterResource(id = R.drawable.dice_1)
+    var result by remember {
+        mutableStateOf(1)
+    }
+    val imageResource = when (result) {
+        1 -> R.drawable.dice_1
+        2 -> R.drawable.dice_2
+        3 -> R.drawable.dice_3
+        4 -> R.drawable.dice_4
+        5 -> R.drawable.dice_5
+        else -> R.drawable.dice_6
+    }
+    val painter = painterResource(id = imageResource)
     Column(
         modifier = modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
-        Image(painter = painter, contentDescription = null)
+        Image(painter = painter, contentDescription = result.toString())
         Button(onClick = {
             result = (1..6).random()
         }) {
